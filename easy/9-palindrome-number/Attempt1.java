6249871eb44e2ba476fa4ddd8ff3cb2ea9a0ed11
Kiko Carisse <kiko@mediabot.ca>
2024-05-25 18:44:10 -0600

    initial commit

---
+++ easy/9-palindrome-number/Attempt1.java
@@ -0,0 +1,12 @@
+
+class Solution {
+	public static void main(String[] args) {
+		isPalindrome();
+	}
+	public static boolean isPalindrome() {
+	
+		System.out.println("Hello world!");
+
+		return true;
+	}
+}
